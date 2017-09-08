1d4a0e3daa812bdc6f2f114bf01b7920abb7e34e
Saheb Singh <sebs1797@gmail.com>
2017-09-09 04:06:07 +0530

    Complete Project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 20News_using_topicmod
+                                                                                                                                                          
+Finding most relatable posts to an input query, using the topics to define similarity.
+
+Database from http://mlcomp.org/datasets/379
---
+++ news_topic_mod.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  9 00:34:59 2017
+
+@author: sahebsingh
+"""
+
+from __future__ import print_function
+
+""" Import all Necessary Modules. """
+
+from scipy.spatial import distance
+import numpy as np
+from gensim import corpora, models
+import sklearn.datasets
+import nltk.stem
+from collections import defaultdict
+import nltk.corpus
+from stop_words import get_stop_words
+
+class DirectText(corpora.textcorpus.TextCorpus):
+
+    def get_texts(self):
+        return self.input
+
+    def __len__(self):
+        return len(self.input)
+
+""" Specifying Stemmers and Stop Words. """
+
+english_stemmer = nltk.stem.SnowballStemmer('english')
+stopwords = set(get_stop_words('english'))
+
+""" Importing the Dataset """
+
+dataset = sklearn.datasets.fetch_20newsgroups(subset = "test")
+
+otexts = dataset.data
+texts = dataset.data
+
+""" Preprocessing data """
+
+texts = [t.split() for t in texts]
+# Converting texts to lower case
+texts = [map(lambda w: w.lower(), t) for t in texts]
+# Filtering texts which has set("+-.?!()>@012345689") in it.
+texts = [filter(lambda s: not len(set("+-.?!()>@012345689") & set(s)), t)
+         for t in texts]
+texts = [filter(lambda s: (len(s) > 3) and (s not in stopwords), t) 
+        for t in texts]
+texts = [map(english_stemmer.stem, t) for t in texts]
+
+
+""" Storing Words as a Dictionary. """
+
+usage = defaultdict(int)
+
+for t in texts:
+    for w in t:
+        usage[w] += 1        
+        
+""" Filtering words """
+
+limit = len(texts)/10
+too_common = [w for w in usage if usage[w]>limit]
+too_common = set(too_common)
+texts = [filter(lambda s: s not in too_common, t) for t in texts]
+
+""" Creating lda topic model """           
+
+corpus = corpora.BleiCorpus('/Users/sahebsingh/Documents/books/Machine Learning/chap4/ap 2/ap.dat',
+'/Users/sahebsingh/Documents/books/Machine Learning/chap4/ap 2/vocab.txt')
+
+model = models.ldamodel.LdaModel(
+                  corpus,
+                  num_topics=100,
+                  id2word=corpus.id2word,
+                  alpha=None)
+
+thetas = np.zeros((len(texts), 100))
+for i, c in enumerate(corpus):
+    for ti, v in model[c]:
+        thetas[i, ti] += v
+
+distances = distance.squareform(distance.pdist(thetas))
+large = distances.max() + 1
+for i in range(len(distances)):
+    distances[i, i] = large
+
+print(otexts[1])
+print()
+print()
+print()
+print(otexts[distances[1].argmin()])
+
+        
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
